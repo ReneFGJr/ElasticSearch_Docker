6c19513789548cabb2b63d94c3f472f198684d99
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-11-13 09:06:38 -0300

    v0.22.11.13

--- start_docker_elastic.sh
+++ start_docker_elastic.sh
@@ -1,1 +1,1 @@
- docker run --name es01 --net elastic -p 9200:9200 -p 9300:9300 -it docker.elastic.co/elasticsearch/elasticsearch:8.5.0
+ docker run -e ES_JAVA_OPTS="-Xms1g -Xmx1g" -e ENROLLMENT_TOKEN="<token>" --name es01 --net elastic -p 9200:9200 -p 9300:9300 -it docker.elastic.co/elasticsearch/elasticsearch:8.5.0
